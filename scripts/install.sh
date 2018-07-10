f85f98783d7cf230b9437d6e9320b384995d2024
rpidanny <abhishekmaharjan1993@gmail.com>
2018-07-10 10:23:12 +0200

    updated install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,28 @@
-echo 'Restoring dotfiles...'
+#!/usr/bin/env bash
 
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+user () {
+  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
+
+info("Restoring dotfiles.")
 dir="$HOME/workspace/personal"
 mkdir -p $dir && cd $dir
 git clone https://github.com/rpidanny/dotfiles.git
 cd dotfiles
+
 # TODO: create symlink-dorfiles.sh
 sh symlink-dotfiles.sh
