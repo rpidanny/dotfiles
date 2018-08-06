ae5237d25dc4c5fb2e573f72f389dea4225a87a5
rpidanny <abhishekmaharjan1993@gmail.com>
2018-08-06 10:02:51 +0200

    switched cuda to 9.0

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,6 +1,6 @@
 export LC_ALL=en_US.UTF-8
-# export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}
-export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
+export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}
+# export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH='/usr/local/cuda/lib64'
 export CUDA_HOME=/usr/local/cuda
 export PATH=$PATH:/usr/local/go/bin
