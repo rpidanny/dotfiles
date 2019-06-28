75852eaf45f653477a9e95d2d1323adcf89d0923
rpidanny <abhishekmaharjan1993@gmail.com>
2019-06-28 13:47:24 +0200

    Remove git plugin

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -66,7 +66,7 @@ ENABLE_CORRECTION="true"
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
 plugins=(
-  git
+  # git
   docker
   docker-compose
   zsh-autosuggestions
